7dbba0aad32edcec2b73e2a6ec4e5e378bf7d0f7
kukuruz-1104 <2827893553@qq.com>
2020-12-19 19:55:26 +0800

    退款接口

--- payment/src/main/java/cn/edu/xmu/payment/service/impl/RefundServiceImpl.java
+++ payment/src/main/java/cn/edu/xmu/payment/service/impl/RefundServiceImpl.java
@@ -1,45 +1,216 @@
-package cn.edu.xmu.payment.service.impl;
-
-import cn.edu.xmu.ooad.util.ResponseCode;
-import cn.edu.xmu.ooad.util.ReturnObject;
-import cn.edu.xmu.payment.model.vo.NewRefundVo;
-import cn.edu.xmu.payment.service.PaymentService;
-import cn.edu.xmu.payment.service.RefundService;
-import cn.edu.xmu.payment.util.RefundStates;
-import org.apache.dubbo.config.annotation.DubboService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@DubboService(version ="0.0.1")
-public class RefundServiceImpl {
-    @Autowired
-    private PaymentService paymentService;
-
-    @Autowired
-    private RefundService refundService;
-
-    //订单退款api  对子订单的现金进行退款
-    /**
-     * @author Yuting Zhong
-     * @param shopId 店铺id
-     * @param pid 父订单id
-     * @param amount  退款金额
-     * created at 2020/12/18
-     */
-    public boolean createOrderRefund(Long shopId,Long pid,Long amount){
-
-        //查询父订单支付
-        Long paymentId=paymentService.getOrderCashPaymentId(pid);
-
-        //构造现金退款newRefunVo
-        NewRefundVo vo=new NewRefundVo();
-        vo.setAmount(amount);
-
-        //调用service层退款
-        ReturnObject refund=refundService.createRefund(shopId,paymentId,vo);
-        if(refund.getCode().equals(ResponseCode.OK)){
-            return true;
-        }
-        return false;
-    }
-
-}
+//package cn.edu.xmu.payment.service.impl;
+//
+//import cn.edu.xmu.ooad.util.ResponseCode;
+//import cn.edu.xmu.ooad.util.ReturnObject;
+//import cn.edu.xmu.order_provider.IOrderService;
+//import cn.edu.xmu.order_provider.other.IPOtherService;
+//import cn.edu.xmu.payment.model.bo.Payment;
+//import cn.edu.xmu.payment.model.po.PaymentPo;
+//import cn.edu.xmu.payment.model.vo.NewRefundVo;
+//import cn.edu.xmu.payment.service.PaymentService;
+//import cn.edu.xmu.payment.service.RefundService;
+//import cn.edu.xmu.payment.util.PaymentPatterns;
+//import cn.edu.xmu.payment.util.RefundStates;
+//import org.apache.dubbo.config.annotation.DubboReference;
+//import org.apache.dubbo.config.annotation.DubboService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//@DubboService(version ="0.0.1")
+//public class RefundServiceImpl {
+//    @Autowired
+//    private PaymentService paymentService;
+//
+//    @Autowired
+//    private RefundService refundService;
+//
+//    @DubboReference(version = "0.0.1",check = false)
+//    private IPOtherService otherSerivice;
+//
+//    @DubboReference(version = "0.0.1",check = false)
+//    private IOrderService orderService;
+//
+//
+//    /**
+//     * 预售订单退款
+//     * @param shopId
+//     * @param customerId
+//     * @param pid
+//     * @param restPrice  预售尾款
+//     * @return  1:退款成功   -2:预售订单支付有问题  -3:找不到尾款支付
+//     */
+//    public Integer preOrderRefund(Long shopId,Long customerId,Long pid,Long restPrice){
+//
+//        //退款金额为0  退款成功  不产生退款记录
+//        if(restPrice==0)
+//            return 1;
+//
+//        //查询两笔定金支付和尾款支付
+//        List<PaymentPo> paymentPos=paymentService.findOrderPayment(pid);
+//        if(paymentPos.isEmpty()||paymentPos==null){
+//            return -2;
+//        }
+//
+//        if(paymentPos.size()!=2)
+//            return -2;
+//
+//        Long firstPay=0L,nextPay=0L;
+//        Long firstPayId=0L,nextPayId=0L;
+//        firstPay=paymentPos.get(0).getAmount();
+//        firstPayId=paymentPos.get(0).getId();
+//        nextPay=paymentPos.get(1).getAmount();
+//        nextPayId=paymentPos.get(1).getAmount();
+//
+//        Long paymentId;
+//        if(firstPay==restPrice)
+//            paymentId=firstPayId;
+//        else if(nextPay==restPrice)
+//            paymentId=nextPayId;
+//        else
+//            return -3;
+//
+//        NewRefundVo vo=new NewRefundVo();
+//        vo.setAmount(restPrice);
+//        //创建退款
+//         ReturnObject refund=refundService.create(shopId,customerId,paymentId,vo);
+//         if(refund.getCode().equals(ResponseCode.OK))
+//             return 1;
+//         else
+//             return -1;
+//    }
+//
+//    /**
+//     * 团购订单退款  只会有一笔支付  对那一笔支付进行退款
+//     * @param shopId
+//     * @param customerId
+//     * @param pid
+//     * @param amount
+//     * @return 1退款成功  0退款金额大于支付金额  -1订单不是团购订单  -2订单未支付
+//     */
+//    public Integer couponRefund(Long shopId,Long customerId,Long pid,Long amount){
+//        //退款金额为0  退款成功 不产生退款记录
+//        if(amount==0)
+//            return 1;
+//
+//        //查询父订单支付
+//        List<PaymentPo> paymentPos=paymentService.findOrderPayment(pid);
+//        if(paymentPos.isEmpty()||paymentPos==null){
+//            return -2;
+//        }
+//
+//        //支付多于一笔支付  不是团购支付
+//        if(paymentPos.size()!=1){
+//            return -1;
+//        }
+//
+//        //退款金额大于支付金额
+//        if(amount>paymentPos.get(0).getAmount()){
+//            return 0;
+//        }
+//
+//        Long paymentId=paymentPos.get(0).getId();
+//        NewRefundVo vo=new NewRefundVo();
+//        vo.setAmount(amount);
+//        //创建支付
+//       ReturnObject refund=refundService.create(shopId,customerId,paymentId,vo);
+//       if(refund.getCode().equals(ResponseCode.OK))
+//           return 1;
+//       else
+//           return -1;
+//    }
+//
+//    /**
+//     * 普通订单退款
+//     * @param shopId
+//     * @param customerId
+//     * @param pid
+//     * @param amount
+//     * @return
+//     */
+//    public Integer normalRefund(Long shopId,Long customerId,Long pid,Long amount){
+//        //退款金额为0 退款成功 但不生成退款记录
+//        if(amount==0)
+//            return 1;
+//
+//        //查询父订单支付
+//        List<PaymentPo> paymentPos=paymentService.findOrderPayment(pid);
+//        if(paymentPos.isEmpty()||paymentPos==null){
+//            return -2;
+//        }
+//
+//        //如果有两次支付 分别拿到两次支付的id和金额   没有校验订单两次以上的支付  因为应该不会存在
+//        Long cashPay=0L,rebatePay=0L;
+//        Long cashPaymentId=0L,rebatePaymentId=0L;
+//
+//        for(PaymentPo pay:paymentPos){
+//            if(pay.getPaymentPattern()== PaymentPatterns.NORMALPAY.getCode()) {
+//                cashPay=pay.getAmount();
+//                cashPaymentId=pay.getId();
+//            }
+//            else if(pay.getPaymentPattern()==PaymentPatterns.REBATEPAY.getCode()){
+//                rebatePay=pay.getAmount();
+//                rebatePaymentId=pay.getId();
+//            }
+//        }
+//
+//        //退款金额应该小于支付金额
+//        if(amount>cashPay+rebatePay)
+//            return 0;
+//
+//        //如果只有返点支付  就退回返点
+//        if(cashPaymentId.equals(0L)&&!rebatePaymentId.equals(0L)){
+//            NewRefundVo voRebate=new NewRefundVo();
+//            voRebate.setAmount(amount);
+//            //创建退款
+//          ReturnObject refundRebate=refundService.create(shopId,customerId,rebatePaymentId,voRebate);
+//          if(!refundRebate.getCode().equals(ResponseCode.OK))
+//              return -1;
+//        }else{//有现金支付 先进行返点退款  之后进行现金退款
+//            Long rebateAmount=0L,cashAmount=0l;
+//            //返点退款
+//            if(amount>cashPay)
+//            {
+//                rebateAmount=amount-cashPay;
+//                cashAmount=cashPay;
+//
+//                NewRefundVo voRebate=new NewRefundVo();
+//                voRebate.setAmount(rebateAmount);
+//                //创建退款
+//               ReturnObject refundRebate=refundService.create(shopId,customerId,rebatePaymentId,voRebate);
+//               if(!refundRebate.getCode().equals(ResponseCode.OK))
+//                   return -1;
+//            }else{
+//                cashAmount=amount;
+//            }
+//
+//
+//            NewRefundVo voCash=new NewRefundVo();
+//            voCash.setAmount(cashAmount);
+//            //创建退款
+//           ReturnObject refundCash=refundService.create(shopId,customerId,cashPaymentId,voCash);
+//           if(!refundCash.getCode().equals(ResponseCode.OK))
+//               return -1;
+//
+//        }
+//        return 1;
+//    }
+//
+//    //售后退款
+//    /**
+//     * 售后退款
+//     * @param shopId
+//     * @param customerId
+//     * @param orderItemId
+//     * @param amount
+//     * @return
+//     */
+//    public Integer aftersaleRefund(Long shopId,Long customerId,Long orderItemId,Integer amount){
+//        Long pid= orderService.getOrderItemPid(orderItemId);
+//
+//        return normalRefund(shopId,customerId,pid,amount.longValue());
+//    }
+//
+//}
